50614cdf229fae3a519fd05f165ff311e127936f
wley3337@gmail.com <wley3337@gmail.com>
2018-05-02 23:36:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,8 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace","Linus","Matz"]
 
 def batch_badge_creator
   # should return a string that has each person's name: "Hello, my name is #{name}"
+  
+  
 end
 
 
