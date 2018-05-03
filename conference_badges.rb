a814adde05cbd9cd5cde4af8afe3e5bf3bd643e7
wley3337@gmail.com <wley3337@gmail.com>
2018-05-03 00:33:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 
 speekers = ["Edsger", "Ada", "Charles", "Alan", "Grace","Linus","Matz"]
-badges = []
-room_assignments = []
+badges1 = []
+room_assignments1 = []
 
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -12,28 +12,28 @@ end
 def batch_badge_creator(attendees)
   # should return a string that has each person's name: "Hello, my name is #{name}"
   attendees.each do |x|
-    badges.push("Hello, my name is #{x}.")
+    badges1.push("Hello, my name is #{x}.")
   end
-  badges
+  badges1
 end
 
 def assign_rooms(attendees)
   #assign rooms 1-7 return a list of room assignments in f the form of: "Hello, #{name}! You'll be assigned to room #{room_number}!"
   
   attendees.each.with_index(1) do |x, index|
-    room_assignments.push("Hello, #{x}! You'll be assigned to room #{index}")
+    room_assignments1.push("Hello, #{x}! You'll be assigned to room #{index}")
   end
-  room_assignments
+  room_assignments1
 end
 
 
 def printer(attendees)
   #outputs the results of #batch_badge_creator (badges) and then #assign_rooms (room_assignments)
   
-  badges.each do |x|
+  badges1.each do |x|
     puts "#{x}" 
   end
-  room_assignments.each do |x|
+  room_assignments1.each do |x|
     puts "#{x}"
   end
 end
